6fdd8e7a2e03326298c484b1ef45e14f5cdc5043
Dmitriy <145620090+iLooToS@users.noreply.github.com>
2024-01-20 02:14:28 +0300

    layout-and-footer

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { LayoutProps } from './Layout.props';
 import React, { FunctionComponent } from 'react';
-import styles from './P.module.css';
+import styles from './Layout.module.css';
 import cn from 'classnames';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Header } from './Header/Header';
@@ -8,16 +8,14 @@ import { Footer } from './Footer/Footer';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
-		<>
-			<Header />
-			<div>
-				<Sidebar />
-				<div>
-					{children}
-				</div>
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body}>
+				{children}
 			</div>
-			<Footer />	
-		</>	
+			<Footer className={styles.footer} />	
+		</div>	
 	);
 };
 
