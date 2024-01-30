b1358a1901b6faa789a788296d5c0baf1c1c30ab
Dmitriy <145620090+iLooToS@users.noreply.github.com>
2024-01-30 13:23:48 +0300

    ssr-getStaticPaths

--- components/Tag/Tag.props.ts
+++ components/Tag/Tag.props.ts
@@ -1,7 +1,7 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 
 
-export interface TagProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+export interface PProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
 	size?: 's' | 'm';
 	children: ReactNode;
 	color?: 'ghost' | 'red' | 'gray' | 'green' | 'primary';
