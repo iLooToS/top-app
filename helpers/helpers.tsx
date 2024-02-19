3b3b1cb2b47c03ea2cd708ef9932f1d3b380dad7
Dmitriy <145620090+iLooToS@users.noreply.github.com>
2024-02-19 12:15:32 +0300

    application-regex

--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -11,3 +11,5 @@ export const firstLevelMenu: FirstLevelMenuItem[] = [
 	{ route: 'bookds', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
 	{ route: 'products', name: 'Продукты', icon: <ProducstIcon />, id: TopLevelCategory.Products },
 ];
+
+export const priceRu = (price: number): string => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ').concat(' ₽');
