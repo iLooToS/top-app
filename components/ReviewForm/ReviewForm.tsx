4c0f90776387368bb97eb1b99c3e84cbe558def1
iLooToS <rhymudienullte@gmail.com>
2024-08-16 23:24:08 +0300

    review-send

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -7,7 +7,10 @@ import { Rating } from "../Rating/Rating";
 import { Textarea } from "../Textarea/Textarea";
 import { Button } from "../Button/Button";
 import { useForm, Controller } from "react-hook-form";
-import { IReviewForm } from "./ReviewForm.interface";
+import { IReviewForm, IReviewSentResponse } from "./ReviewForm.interface";
+import axios from "axios";
+import { API } from "@/helpers/api";
+import { useState } from "react";
 
 export const ReviewForm = ({
   productId,
@@ -19,10 +22,27 @@ export const ReviewForm = ({
     control,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<IReviewForm>();
 
-  const onSubmit = (data: IReviewForm) => {
-    console.log(data);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [isError, setIsError] = useState<string>();
+
+  const onSubmit = async (formData: IReviewForm) => {
+    try {
+      const { data } = await axios.post<IReviewSentResponse>(
+        API.review.createDemo,
+        { ...formData, productId }
+      );
+      if (data.message) {
+        setIsSuccess(true);
+        reset();
+      } else {
+        setIsError("Что-то пошло не так");
+      }
+    } catch (e) {
+      setIsError((e as Error).message);
+    }
   };
 
   return (
@@ -55,7 +75,7 @@ export const ReviewForm = ({
                 rating={field.value}
                 ref={field.ref}
                 setRating={field.onChange}
-				error={errors.rating}
+                error={errors.rating}
               />
             )}
           />
@@ -76,11 +96,19 @@ export const ReviewForm = ({
           </span>
         </div>
       </div>
-      <div className={styles.success}>
-        <div className={styles.successTitle}>Ваш отзыв отправлен</div>
-        <div>Спасибо, ваш отзыв будет опубликован после проверки.</div>
-        <CloseIcon className={styles.close} />
-      </div>
+      {isSuccess && (
+        <div className={cn(styles.success, styles.panel)}>
+          <div className={styles.successTitle}>Ваш отзыв отправлен</div>
+          <div>Спасибо, ваш отзыв будет опубликован после проверки.</div>
+          <CloseIcon className={styles.close} onClick={()=> setIsSuccess(false)}/>
+        </div>
+      )}
+      {isError && (
+        <div className={cn(styles.error, styles.panel)}>
+          Что-то пошло не так, попробуйте обновить страницу.
+          <CloseIcon className={styles.close} onClick={()=> setIsError(undefined)}/>
+        </div>
+      )}
     </form>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,58 @@
-import { withLayout } from '@/layout/Layout';
-import { Button, Htag, P, Tag, Rating, Input, Textarea } from '../components';
-import React, { useState } from 'react';
-import { GetStaticProps } from 'next';
-import axios from 'axios';
-import { MenuItem } from '@/interfaces/menu.interface';
+import { withLayout } from "@/layout/Layout";
+import { Button, Htag, P, Tag, Rating, Input, Textarea } from "../components";
+import React, { useState } from "react";
+import { GetStaticProps } from "next";
+import axios from "axios";
+import { MenuItem } from "@/interfaces/menu.interface";
+import { API } from "@/helpers/api";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function Home({ menu }: HomeProps): JSX.Element {
-	const [rating, setRating] = useState<number>(4);
+  const [rating, setRating] = useState<number>(4);
 
-	return (
-		<>
-			<Htag tag='h1'>Заголовок</Htag>
-			<Button apperance='primary' arrow='right'>Кнопка</Button>
-			<Button apperance='ghost' arrow='down'>Кнопка</Button>
-			<P size='l'>Большой</P>
-			<P>Средний</P>
-			<P size='s'>Маленький</P>
-			<Tag size='s'>Ghost</Tag>
-			<Tag size='m' color='red'>Red</Tag>
-			<Tag size='s' color='green'>Green</Tag>
-			<Tag color='primary'>Primary</Tag>
-			<Rating rating={rating} isEditable setRating={setRating} />
-			<Input placeholder='тест' />
-			<Textarea placeholder='тест area' />
-		</>
-	);
+  return (
+    <>
+      <Htag tag="h1">Заголовок</Htag>
+      <Button apperance="primary" arrow="right">
+        Кнопка
+      </Button>
+      <Button apperance="ghost" arrow="down">
+        Кнопка
+      </Button>
+      <P size="l">Большой</P>
+      <P>Средний</P>
+      <P size="s">Маленький</P>
+      <Tag size="s">Ghost</Tag>
+      <Tag size="m" color="red">
+        Red
+      </Tag>
+      <Tag size="s" color="green">
+        Green
+      </Tag>
+      <Tag color="primary">Primary</Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
+      <Input placeholder="тест" />
+      <Textarea placeholder="тест area" />
+    </>
+  );
 }
 
 export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-	const firstCategory = 0;
-	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
-		firstCategory
-	});
-	return {
-		props: {
-			menu,
-			firstCategory
-		}
-	};
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+    firstCategory,
+  });
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
 };
 
 interface HomeProps extends Record<string, unknown> {
-	menu: MenuItem[],
-	firstCategory: number;
+  menu: MenuItem[];
+  firstCategory: number;
 }
