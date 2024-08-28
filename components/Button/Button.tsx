637a81a7686fa1da67a33b42752ef1454cfa6e93
iLooToS <rhymudienullte@gmail.com>
2024-08-28 18:34:37 +0300

    mobile-menu

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,23 +1,43 @@
-import styles from './Button.module.css';
-import { ButtonProps } from './button.props';
-import ArrowIcon from './arrow.svg';
-import cn from 'classnames';
+import styles from "./Button.module.css";
+import { ButtonProps } from "./button.props";
+import ArrowIcon from "./arrow.svg";
+import cn from "classnames";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
 
-export const Button = ({ apperance, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
-	return (
-		<button
-			className={cn(styles.button, className, {
-				[styles.primary]: apperance == 'primary',
-				[styles.ghost]: apperance == 'ghost',
-			})}
-			{...props}
-		>
-			{children}
-			{arrow != 'none' && <span className={cn(styles.arrow, {
-				[styles.down]: arrow == 'down'
-			})}>
-				<ArrowIcon />
-			</span>}
-		</button>
-	);
+export const Button = ({
+  apperance,
+  arrow = "none",
+  children,
+  className,
+  ...props
+}: ButtonProps): JSX.Element => {
+  const scale = useMotionValue(1);
+
+  useEffect(() => {
+    scale.onChange((s) => console.log(s));
+  }, []);
+
+  return (
+    <motion.button
+      whileHover={{ scale: 1.05 }}
+      className={cn(styles.button, className, {
+        [styles.primary]: apperance == "primary",
+        [styles.ghost]: apperance == "ghost",
+      })}
+      style={{ scale }}
+      {...props}
+    >
+      {children}
+      {arrow != "none" && (
+        <span
+          className={cn(styles.arrow, {
+            [styles.down]: arrow == "down",
+          })}
+        >
+          <ArrowIcon />
+        </span>
+      )}
+    </motion.button>
+  );
 };
